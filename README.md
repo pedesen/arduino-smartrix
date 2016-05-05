4c929a9d0f065d6ad5c9e26a9aa57425fbd15c8c
pedesen <patrick.dehn@camunda.com>
2016-05-05 16:55:37 +0200

    rework & documentation

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # arduino-smartrix
-Arduino + LED Matrix = ❤
+A message protocol for turning on or off LEDs in up to 16 MAX72xx matrices.
+
+Info about the wiring: http://playground.arduino.cc/Main/MAX72XXHardware
+
+## Message structure
+
+One message contains information for up to 64 LEDs in a Matrix connected to one MAX72xx device (up to 16 devices are addressable)
+
+* Byte 1: A hex char identifying the MAX72xx device (0 to 15)
+* Byte 2: A hex char defining the number of rows (up to 8)
+* Byte 3: A hex char defining the number of columns (up to 8)
+* Bytes 4 to 19: Each byte is a hex char containing status information (on/off) for 4 LEDs (1 to 16 bytes)
+* The last byte is a termination character (`;`)
+
+## Example for a 4x3 LED Matrix
+
+`1`: LED is on
+
+`0`: LED is off
+
+```
+[[ 0, 0, 1 ],
+ [ 0, 1, 0 ],
+ [ 1, 1, 1 ],
+ [ 0, 0, 0 ]]
+```
+
+Binary representation of the matrix:
+`0010 1011 1000`
+
+Hex representation of the matrix:
+`2 B 8`
+
+Serial message sent to the arduino:
+`0432B8;`
+
+The first byte (`0`) addresses the first MAX72xx device, the second byte (`5`) defines a number of 5 rows, the third byte (`8`) defines a number of 8 columns, bytes 4 to 6 (`2B8`) are the hex representation of the 4x3 matrix (see above). The last byte (`;`) is the termination byte.
+
+## Example for a 8x8 LED Matrix
+
+To turn on all LEDs of a 8x8 Matrix send this message to the Arduino
+
+`088FFFFFFFFFFFFFFFF;`
--- smartrix/smartrix.ino
+++ smartrix/smartrix.ino
@@ -1,53 +1,57 @@
 #include "LedControl.h"
 
-LedControl lc=LedControl(12,11,10,1); 
+LedControl lc=LedControl(12,11,10,1);
 
 void setup() {
   lc.shutdown(0,false);
   lc.setIntensity(0,15);
-  
+
   Serial.begin(9600);
   Serial.println("Ready");
 }
 
+// Return the correct byte representation of an ASCII hex char
+// Example: inChar = 'D' = 68  -->  13 = 1101
 byte getByte(char inChar) {
-  // returns the correct byte representation of an ASCII hex char
-  // Example: inChar = 'D' = 68  -->  13 = 1101
   return inChar < 58 ? (inChar - 48) : (inChar - 55);
 }
 
 void loop() {
-  
+
   if (Serial.available()) {
-  
-    char ledBuffer[10];
-    
-    // read the first byte - a hex char idenifying the device (0-15)
-    byte device = getByte(Serial.read());
-    
-    // read the next 10 bytes - each byte is a hex char which contains
-    // the binary status of four LEDs. Example: D = 1101
-    Serial.readBytes(ledBuffer, 10);
-    
-    boolean leds[40];
-    
-    for(int i=0; i<10; i++) {
-      // for each char in the ledBuffer
-      // get the byte value
-      byte ledByte = getByte(ledBuffer[i]);
-      
+
+    char buffer[19];
+
+    Serial.readBytesUntil(';', buffer, 19);
+
+    // read the first byte - a hex char identifying the device (0-15)
+    byte device = getByte(buffer[0]);
+
+    // read the second byte - a hex char defining the number of rows
+    byte num_rows = getByte(buffer[1]);
+
+    // read the third byte - a hex char defining the number of columns
+    byte num_cols = getByte(buffer[2]);
+
+    // the number of bytes (must be a divisor of 4)
+    byte num_bytes = ceil(float(num_rows * num_cols)/4);
+
+    boolean leds[num_bytes*4];
+
+    // read all remaining bytes
+    for(int i=0; i<num_bytes; i++) {
+      byte ledByte = getByte(buffer[i+3]);
+
+      // for the first four bits of the byte get the boolean value (0 or 1)
       for(int j=0; j<4; j++) {
-        // for the first four bits of the byte
-        // get the boolean value (0 or 1)
-        leds[(i*4)+j] = bitRead(ledByte, j);
+        leds[(i*4)+j] = bitRead(ledByte, 3-j);
       }
     }
-    
-    for(int i=0; i<8; i++) {
-      // for each row i...
-      for(int j=0; j<5; j++) {
-        // for each column j...
-        lc.setLed(device, i, j, leds[(i*5)+j]);
+
+    // iterate over rows and columns and set the LEDs
+    for(int row=0; row<num_rows; row++) {
+      for(int col=0; col<num_cols; col++) {
+        lc.setLed(device, row, col, leds[(row*num_cols)+col]);
       }
     }
   }
